92e9220ec7e9ecd4b6d68909c00caf26c661532d
Abel Binoop <abelbinoop@live.co.uk>
2019-06-13 17:52:45 +0100

    Fix issue #1

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import hashlib
 from getpass import getpass
 from base64 import a85encode
 
-print('''Welcome to PyPass, a password manager written in Python!
+
+def genpasswd(website, masterpass):
+    website = website.encode()
+    masterpass = masterpass.encode()
+    salted = website + masterpass
+    hash = hashlib.sha256(salted).digest()
+    password = a85encode(hash).decode()
+
+    print('This is your password: ', password)
+
+
+def main():
+    print('''Welcome to PassMan, a password manager written in Python!
 Please type in the name of the website you want to sign up/log in to:''')
-website = input().encode()
-print('Now type in the Master Password:')
-masterpass = getpass('').encode()
+    website = input().encode()
+    print('Now type in the Master Password:')
+    masterpass = getpass('').encode()
+    genpasswd(website, masterpass)
 
-salted = website + masterpass
-hash = hashlib.sha256(salted).digest()
-password = a85encode(hash).decode()
 
-print('This is your password: ', password)
+if __name__ == '__main__':
+    main()
