ba45b19f7449b4c74a2838ae30ad309119a30246
Abel Binoop <abelbinoop@live.co.uk>
2019-06-13 18:27:59 +0100

    Add docstrings and rename main.py

--- passman.py
+++ passman.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
+"""
+Generate/retrieve and manage passwords.
+
+Usage:
+    ./main.py
+"""
 import hashlib
 from getpass import getpass
 from base64 import a85encode
 
 
 def genpasswd(website, masterpass):
+    """
+    Generate/retrieve a password using the website name and Master Passphrase.
+
+    Args:
+        website: The name of the website you want to sign up/log in to.
+        masterpassword: Your Master Passphrase.
+
+    Outputs:
+        The password, generated from the website name and Master Passphrase.
+    """
     website = website.encode()
     masterpass = masterpass.encode()
     salted = website + masterpass
@@ -15,6 +31,9 @@ def genpasswd(website, masterpass):
 
 
 def main():
+    """
+    Input the website name and Master Passphrase to generate/retrieve a password.
+    """
     print('''Welcome to PassMan, a password manager written in Python!
 Please type in the name of the website you want to sign up/log in to:''')
     website = input().encode()
