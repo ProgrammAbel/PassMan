2cc12672174ab337aa4a319f128bed8d53eba4b1
Abel Binoop <abelbinoop@live.co.uk>
2019-06-14 08:29:17 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PassMan",
+    version="0.7",
+    packages=find_packages(),
+    scripts=['passman.py'],
+
+    author="Abel Binoop",
+    author_email="abelbinoop@live.co.uk",
+    description="A different kind of password manager.",
+    license="GPL3",
+    keywords="password manager python",
+)
