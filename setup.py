5dbfb2c988dc7a32e2cdfb0f47e3a1893da3deb7
Abel Binoop <abelbinoop@live.co.uk>
2019-06-15 14:11:54 +0100

    Change version to accompany 15 char limit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PassMan",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     scripts=['passman'],
     data_files=[
