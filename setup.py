3f247dee3a168be86e8cb2e5231e13199992c6f0
Abel Binoop <abelbinoop@live.co.uk>
2019-07-15 18:38:53 +0100

    Add password gen on RETURN in entries

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PassMan",
-    version="1.3",
+    version="1.4",
     packages=find_packages(),
     scripts=['passman'],
     data_files=[
